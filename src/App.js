44d137c75394992c9d985f459898e5f22f4e9312
Migara <evamarcod@gmail.com>
2019-09-08 20:25:49 +0200

    Add getRandomNumber() to state

--- src/App.js
+++ src/App.js
@@ -4,78 +4,63 @@ import './scss/main.scss';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.checkNumbers = this.checkNumbers.bind(this);
+    this.getUserName = this.getUserName.bind(this);
     this.state = {
-      message : 'Escribe un número y dale a prueba',
-      RandomNumber : 45,
-      ValueNumber : 0
+      clue : 'Escribe un número y dale a prueba',
+      RandomNumber : this.getRandomNumber(100),
+      ValueNumber : 0,
+      counter : 0
     }
+    console.log('r', this.state.RandomNumber);
   }
-  // getRandomNumber(max) {
-  //   const number = Math.ceil(Math.random() * max);
-  //   this.setState({
-  //     RandomNumber: number
-  //   });
-  //   console.log(number);
-  // }
+  getRandomNumber(max) {
+    const number = Math.ceil(Math.random() * max);
+    return number
+  }
 
-  handleChange(event){
+  getUserName(event){
     const valueNumber = parseInt(event.target.value);
     this.setState(
       {
         ValueNumber: valueNumber
       }
     );
-    console.log(valueNumber);
   }
 
-  handleClick(){
+  checkNumbers(){
     const ValueNumber = this.state.ValueNumber;
     const RandomNumber =this.state.RandomNumber;
+    let counter = this.state.counter;
+    let result;
 
     if(ValueNumber < RandomNumber){
-      console.log(ValueNumber);
-      console.log(RandomNumber);
-      console.log('soy mayor');
-      this.setState(
-        {
-          message : 'El número que has introducido es muy pequeño, introduce un número mayor.'
-        }
-      );
+      result = 'El número que has introducido es muy pequeño, introduce un número mayor.';
+      counter ++;
     }
     else if(ValueNumber > RandomNumber){ 
-      console.log(ValueNumber);
-      console.log(RandomNumber);       
-      console.log('soy menor');
-      this.setState(
-        {
-          message : 'El número que has introducido es muy grande, introduce un número menor.'
-        }
-      );
+      result =  'El número que has introducido es muy grande, introduce un número menor.';
+      counter ++;
     }
     else{
-      console.log(ValueNumber);
-      console.log(RandomNumber);
-      console.log('Has ganado');
-      this.setState(
-        {
-          message : 'Has acertado campeona'
-        }
-      );
+      result = 'Has acertado campeona';
     }
+
+    this.setState({
+      clue: result,
+      counter: counter
+    })
   }
 
   render() {
-    // this.getRandomNumber(100);
-    const handleClick = this.handleClick;
-    const handleChange = this.handleChange;
+    const checkNumbers = this.checkNumbers;
+    const getUserName = this.getUserName;
     return (
       <div className="App">
         <h1 className = "main__title">
           Adivina el número
         </h1>
-        <p className = "tries_text" >0</p>    
+        <p className = "tries_text" >{this.state.counter}</p>    
         <div className="InputDiv">
           <label htmlFor = "input" className = "main__subtitle">
             Introduce aquí tu número.
@@ -85,13 +70,13 @@ class App extends React.Component {
             name="input"
             id="input"
             className="input js__input"
-            onChange = {handleChange}
+            onChange = {getUserName}
           />
-          <button className="btn js__btn" onClick={handleClick}>
+          <button className="btn js__btn" onClick={checkNumbers}>
             Prueba
           </button>
         </div>
-        <p className="resultText">{this.state.message}</p>
+        <p className="resultText">{this.state.clue}</p>
       </div>
     );
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import '../scss/main.scss';
 class Result extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <p className="resultText">Introduce un número y dale a Prueba</p>
